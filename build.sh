9839cc93027be16eb839c527ddc683275af0fb37
Murilo Cadanus da Costa <murilocadanus@gmail.com>
2017-12-07 17:44:38 -0500

    [FIX] Cleaning and showing output whe compiling

--- build.sh
+++ build.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-(cd project && haxelib run hxcpp Build.xml -Diphoneos -DHXCPP_ARM   -DHXCPP_CLANG -DOBJC_ARC)
-(cd project && haxelib run hxcpp Build.xml -Diphoneos -DHXCPP_ARMV7 -DHXCPP_CLANG -DOBJC_ARC)
-(cd project && haxelib run hxcpp Build.xml -Diphoneos -DHXCPP_ARM64 -DHXCPP_CLANG -DOBJC_ARC)
+if [ -d "ndll" ]; then
+    rm -r ndll
+fi
+if [ -d "obj" ]; then
+    rm -r obj
+fi
+
+(cd native && haxelib run hxcpp Build.xml -Diphoneos -DHXCPP_ARM   -DHXCPP_CLANG -DOBJC_ARC $1)
+(cd native && haxelib run hxcpp Build.xml -Diphoneos -DHXCPP_ARMV7 -DHXCPP_CLANG -DOBJC_ARC $1)
+(cd native && haxelib run hxcpp Build.xml -Diphoneos -DHXCPP_ARM64 -DHXCPP_CLANG -DOBJC_ARC $1)
+(cd native && haxelib run hxcpp Build.xml -Diphonesim -DHXCPP_CPP11 -DHXCPP_CLANG -DOBJC_ARC $1)
+(cd native && haxelib run hxcpp Build.xml -Diphonesim -DHXCPP_CPP11 -DHXCPP_M64 -DHXCPP_CLANG -DOBJC_ARC $1)
